58cbff13c811d31abc9f40b20eeed456eb6ed14e
Michael <michaelchao96@hotmail.com>
2023-06-15 14:49:42 -0400

    html hotfixes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,15 +1,18 @@
 import logo from "../../assets/logo/Logo-2.png";
+import { Link } from "react-router-dom";
 import "./Nav.scss";
 
 const Nav = () => {
   return (
     <nav className="nav">
       <div className="nav__logo-wrapper">
-        <img className="nav__logo" src={logo} alt="Paw-fect logo" />
+        <Link to={"/"}>
+          <img className="nav__logo" src={logo} alt="Paw-fect logo" />
+        </Link>
       </div>
       <div className="nav__wrapper">
         <h3 className="nav__link">About us</h3>
-        <h3 className="nav__link">Login</h3>
+        <h3 className="nav__link">SignUp/Login</h3>
       </div>
     </nav>
   );
