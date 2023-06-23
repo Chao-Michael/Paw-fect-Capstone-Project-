7aaa414942f27efab5533ff23290b30dbe12e6b3
Michael <michaelchao96@hotmail.com>
2023-06-23 15:25:51 -0400

    scss styling and scss fixes

--- client/src/components/DogPage/DogPage.js
+++ client/src/components/DogPage/DogPage.js
@@ -5,9 +5,11 @@ const DogPage = ({ data }) => {
   return (
     <section className="dog-page">
       <h3 className="dog-page__header">The Paw-fect dog for you!</h3>
-      {data.map((dog, index) => (
-        <DogCard key={index} image={dog.image_link} name={dog.name} goodWithKids={dog.good_with_children} goodWithdogs={dog.good_with_other_dogs} playfulness={dog.playfulness} />
-      ))}
+      <div className="dog-page__box">
+        {data.map((dog, index) => (
+          <DogCard key={index} image={dog.image_link} name={dog.name} goodWithKids={dog.good_with_children} goodWithdogs={dog.good_with_other_dogs} playfulness={dog.playfulness} />
+        ))}
+      </div>
     </section>
   );
 };
--- client/src/pages/Favourites/Favourites.js
+++ client/src/pages/Favourites/Favourites.js
@@ -19,15 +19,18 @@ const Favourites = () => {
   }, []);
 
   return (
-    <article className="favourites-box">
+    <article className="favourites">
+      <h2 className="favourites__header">It all comes down to these furry friends to be your paw-fect dog!</h2>
       {favouriteList.map(dog => (
-        <div className="favourites">
-          <img className="favourites__image" src={dog.image_url} alt="Photo of the dog" />
-          <div className="favourites__textbox">
-            <h3 className="favourites__name">{dog.dog_name}</h3>
-            <p className="favourites__info">Good with kids: {dog.good_with_kids}</p>
-            <p className="favourites__info">Good with other dogs: {dog.good_with_dogs}</p>
-            <p className="favourites__info">playfulness: {dog.playfulness}</p>
+        <div className="favourites-box">
+          <div className="favourites-container">
+            <img className="favourites-container__image" src={dog.image_url} alt="Photo of the dog" />
+            <div className="favourites-container__textbox">
+              <h3 className="favourites-container__name">{dog.dog_name}</h3>
+              <p className="favourites-container__info">Good with kids: {dog.good_with_kids}</p>
+              <p className="favourites-container__info">Good with other dogs: {dog.good_with_other_dogs}</p>
+              <p className="favourites-container__info">playfulness: {dog.playfulness}</p>
+            </div>
           </div>
         </div>
       ))}
